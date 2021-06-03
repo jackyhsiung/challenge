4feb3a58e84138214898187e6bd5f2d75ffd65a0
Jacky Hsiung <forlol1123@gmail.com>
2021-06-03 00:14:11 -0400

    fix getListOfAgesOfUsersWith

--- src/database/index.js
+++ src/database/index.js
@@ -7,28 +7,51 @@ const db = require('./db.js');
 //----------------
 // This is a mock db call that waits for # milliseconds and returns
 const mockDBCall = (dataAccessMethod) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(dataAccessMethod());
-        }, 500);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(dataAccessMethod());
+    }, 500);
+  });
 };
 
 // MOCK DB CALLS
 //----------------
 const getUsers = () => {
-    const dataAccessMethod = () => _.map(db.usersById, userInfo => userInfo)
-    return mockDBCall(dataAccessMethod);
+  const dataAccessMethod = () => _.map(db.usersById, userInfo => userInfo)
+  return mockDBCall(dataAccessMethod);
 };
 
 const getListOfAgesOfUsersWith = (item) => {
-    const dataAccessMethod = () => {
-        // fill me in :)
-    }
-    return mockDBCall(dataAccessMethod);
+  console.log('hello')
+  const dataAccessMethod = () => {
+    // fill me in :)
+
+    const result = {} // map age to count
+    
+    // userArray 
+    const userArray = _.map(db.usersById, userInfo => userInfo)
+    
+    Object.keys(db.itemsOfUserByUsername).map(name => {
+      // first see who has the item
+      if (db.itemsOfUserByUsername[name].includes(item)) {
+        console.log(name, 'has the item')
+        const userAge = userArray.find(user => user.username === name).age
+        console.log(name, 'is of age', userAge)
+        if (result.hasOwnProperty(userAge)) {
+          result[userAge] += 1
+        } else {
+          result[userAge] = 1
+        }
+
+      }
+    })
+    console.log('result', result)
+    return result
+  }
+  return mockDBCall(dataAccessMethod);
 }
 
 module.exports = {
-    getUsers,
-    getListOfAgesOfUsersWith
+  getUsers,
+  getListOfAgesOfUsersWith
 };
